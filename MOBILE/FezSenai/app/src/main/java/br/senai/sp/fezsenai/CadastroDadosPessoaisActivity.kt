d9ff81fcb29be0b25e9283d13c83c3ec0bf2b9fb
DIEGO AUGUSTO FARIAS SILVA <18175158@CFP127ALUNO.LOCAL>
2019-12-10 18:10:21 -0300

    Step 1 estruturado

---
+++ MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/CadastroDadosPessoaisActivity.kt
@@ -0,0 +1,12 @@
+package br.senai.sp.fezsenai
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroDadosPessoaisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_dados_pessoais)
+    }
+}
