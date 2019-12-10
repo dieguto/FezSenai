c4a6cca4fe62f5e06ef56d048e4ae08208d66d4b
KelvinRonaldo <kelvin.ronaldo@hotmail.com>
2019-12-10 18:55:32 -0300

    Finalizar tela de cadastro passo 2

--- MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/MainActivity.kt
+++ MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.senai.sp.fezsenai
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cadastroStep2 = Intent(this, CadastroSeuCurso::class.java)
+        startActivity(cadastroStep2)
+
     }
 }
