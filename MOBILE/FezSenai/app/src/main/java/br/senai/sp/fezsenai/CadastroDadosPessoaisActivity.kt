65af887e719da63b14216a0962d1de6d68e5133b
DIEGO AUGUSTO FARIAS SILVA <18175158@CFP127ALUNO.LOCAL>
2019-12-11 17:17:31 -0300

    Terminado a primeira Step 1

--- MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/CadastroDadosPessoaisActivity.kt
+++ MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/CadastroDadosPessoaisActivity.kt
@@ -1,12 +1,21 @@
 package br.senai.sp.fezsenai
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro_dados_pessoais.*
 
 class CadastroDadosPessoaisActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_dados_pessoais)
+
+        prosseguir.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+
+        }
     }
 }
