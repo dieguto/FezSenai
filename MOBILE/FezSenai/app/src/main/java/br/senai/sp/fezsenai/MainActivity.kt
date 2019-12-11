bb329fe74386040efe5a3df48d567ed13b2d236f
EVELYN FERNANDES ROCHA <18263786@cfp127aluno.local>
2019-12-11 17:21:05 -0300

    perfilPublico CardView

--- MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/MainActivity.kt
+++ MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/MainActivity.kt
@@ -1,12 +1,25 @@
 package br.senai.sp.fezsenai
 
+import android.content.ComponentCallbacks2
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btPerfilPublico.setOnClickListener {
+            val intent = Intent(this, PerfilPublico::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
---
+++ MOBILE/FezSenai/app/src/main/java/br/senai/sp/fezsenai/PerfilPublico.kt
@@ -0,0 +1,12 @@
+package br.senai.sp.fezsenai
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilPublico : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_publico)
+    }
+}
